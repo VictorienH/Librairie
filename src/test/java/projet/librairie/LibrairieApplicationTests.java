3b5bbb3b257fbe7d26087117f7f2a30e7a9dd4a1
VictorienH <henon.victorien@yahoo.com>
2024-02-03 17:13:20 +0100

    First

---
+++ src/test/java/projet/librairie/LibrairieApplicationTests.java
@@ -0,0 +1,13 @@
+package projet.librairie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrairieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
