c8beb2f443962b9d542932e391d6ba0d9fb08807
VictorienH <henon.victorien@yahoo.com>
2024-02-27 22:33:46 +0100

    ajout recherche title/author

--- src/main/java/projet/librairie/controller/LivreController.java
+++ src/main/java/projet/librairie/controller/LivreController.java
@@ -3,6 +3,7 @@ package projet.librairie.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import projet.librairie.service.LivreService;
@@ -21,4 +22,16 @@ public class LivreController {
     public ResponseEntity<String> getLivres() {
         return livreService.getAllLivres();
     }
+
+    @GetMapping("/books/{title}")
+    public ResponseEntity<String> getLivreByName(@PathVariable String title ) {
+        return livreService.getLivresByName(title);
+    }
+
+    @GetMapping("/auteur/{author}")
+    public ResponseEntity<String> getLivreByAuthor(@PathVariable String author ) {
+        return livreService.getLivresByAuthor(author);
+    }
+
+    
 }
--- src/main/java/projet/librairie/service/LivreService.java
+++ src/main/java/projet/librairie/service/LivreService.java
@@ -5,21 +5,52 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
 public class LivreService {
 
-    private final String OPEN_LIBRARY_URL = "https://www.googleapis.com/books/v1/volumes?q=search+terms";
+    private final String OPEN_LIBRARY_URL = "https://www.googleapis.com/books/v1/volumes";
 
     @Autowired
     private RestTemplate restTemplate;
 
     public ResponseEntity<String> getAllLivres() {
         return restTemplate.exchange(
-                OPEN_LIBRARY_URL,
+                "https://www.googleapis.com/books/v1/volumes?q=search+terms",
                 HttpMethod.GET,
                 null,
                 String.class
         );
     }
+
+    public ResponseEntity<String> getLivresByName(String title) {
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(OPEN_LIBRARY_URL)
+                .queryParam("q", "intitle:" + title);
+
+        String url = builder.toUriString();
+        return restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                null,
+                String.class
+        );
+    }
+
+    public ResponseEntity<String> getLivresByAuthor(String author) {
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(OPEN_LIBRARY_URL)
+                .queryParam("q", "inauthor:" + author);
+
+        String url = builder.toUriString();
+        
+        return restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                null,
+                String.class
+        );
+        
+    }
+
+    
 }
